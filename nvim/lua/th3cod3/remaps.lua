1b13beb65f2745df1fb01a2c98fdf04de90c4ea1
Th3Cod3 <yefri@th3cod3.com>
2025-03-08 23:07:31 +0100

    remap macros record

--- nvim/lua/th3cod3/remaps.lua
+++ nvim/lua/th3cod3/remaps.lua
@@ -2,9 +2,11 @@ vim.g.mapleader = ' '
 
 local map = vim.keymap.set
 
+-- Move lines
 map('v', 'K', ":m '<—2<CR>gv=gv")
 map('v', 'J', ":m '>+1<CR>gv=gv")
 
+-- center pane on some movement
 map('n', 'J', "mzJ'z")
 map('n', '<C—d>', '<C—d>zz')
 map('n', '<C—u>', '<C—u>zz')
@@ -13,12 +15,14 @@ map('n', 'N', 'Nzzzv')
 map('v', '<', '<gv')
 map('v', '>', '>gv')
 
+-- System clipboard
 map({ 'n', 'v' }, '<leader>y', [["+y]], { desc = 'Yank to system clipboard' })
 map('n', '<leader>Y', [["+Y]], { desc = 'Yank to system clipboard' })
 map({ 'n', 'v' }, '<leader>p', [["+p]], { desc = 'Paste from system clipboard' })
 map({ 'n', 'v' }, '<leader>P', [["+P]], { desc = 'Paste from system clipboard' })
+map({ 'n', 'v' }, '<leader>x', [["_d]], { desc = 'Delete without yanking' })
 
-map({ 'n', 'v' }, 'D', [["_d]], { desc = 'Delete without yanking' })
+-- Search
 map(
   'n', '<leader>sw', [[:%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>]],
   { desc = 'Replace word under cursor' }
@@ -27,6 +31,12 @@ map({ 'n', 'v' }, '<leader>sh', ':noh<cr>', { desc = 'Clear search highlight' })
 
 map({ 'n', 'v' }, '<leader>kv', ':qa!<cr>', { desc = 'Force quit all file (kill vim)' })
 
+-- Macros
+map('n', 'q', '<nop>', { noremap = true })
+map('n', 'Q', 'q', { noremap = true, desc = 'Record macro' })
+map('n', '<M-q>', 'Q', { noremap = true, desc = 'Replay last register' })
+
+-- Quickfix
 local function toggle_quickfix()
   local windows = vim.fn.getwininfo()
   for _, win in pairs(windows) do
