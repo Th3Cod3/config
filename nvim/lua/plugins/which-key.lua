fbc249e8a3742a44624b84a5a25c49fe2a44c432
Th3Cod3 <yefri@th3cod3.com>
2025-11-20 17:29:11 +0100

    install transfer to sync scp/ssh

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -16,5 +16,42 @@ return {
       { '<auto>', mode = 'nixsotc' },
       { 'a', mode = { 'n', 'v' } },
     },
+    config = function(_, opts)
+      local wk = require('which-key')
+      wk.setup(opts)
+      require('which-key').add({
+        { '<leader>u', group = 'Upload / Download', icon = '' },
+        {
+          '<leader>ud',
+          '<cmd>TransferDownload<cr>',
+          desc = 'Download from remote server (scp)',
+          icon = { color = 'green', icon = '󰇚' },
+        },
+        {
+          '<leader>uf',
+          '<cmd>DiffRemote<cr>',
+          desc = 'Diff file with remote server (scp)',
+          icon = { color = 'green', icon = '' },
+        },
+        {
+          '<leader>ui',
+          '<cmd>TransferInit<cr>',
+          desc = 'Init/Edit Deployment config',
+          icon = { color = 'green', icon = '' },
+        },
+        {
+          '<leader>ur',
+          '<cmd>TransferRepeat<cr>',
+          desc = 'Repeat transfer command',
+          icon = { color = 'green', icon = '󰑖' },
+        },
+        {
+          '<leader>uu',
+          '<cmd>TransferUpload<cr>',
+          desc = 'Upload to remote server (scp)',
+          icon = { color = 'green', icon = '󰕒' },
+        },
+      })
+    end,
   },
 }
